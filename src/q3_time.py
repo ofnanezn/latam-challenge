ee1f67843670b4c372faa460338c249971fd4629
ofnanezn <ofnanezn@gmail.com>
2026-01-15 09:54:35 -0500

    Add documentation for Q3 files

--- src/q3_time.py
+++ src/q3_time.py
@@ -3,14 +3,16 @@ from typing import List, Tuple
 from src.utils import build_dataframe_time
 
 def q3_time(file_path: str) -> List[Tuple[str, int]]:
+    # Assuming beforehand we know the columns we need for the exercise 
     required_cols = ["mentionedUsers"]
 
     df = build_dataframe_time(file_path, required_cols)
 
-    df = df.explode('mentionedUsers')
-    df['mentionedUsers'] = df['mentionedUsers'].str['username']
+    df = df.explode('mentionedUsers') # Flatmap operation on the list of mentioned users
+    df['mentionedUsers'] = df['mentionedUsers'].str['username'] # Get username from user struct 
 
-    top_users = df.value_counts().sort_values(ascending=False).head(10)
+    top_users = df.value_counts().sort_values(ascending=False).head(10) # Count user mentions and get top 10
 
+    # Return in the required format
     return list(top_users.reset_index().itertuples(index=None, name=None))
 
