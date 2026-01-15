03a2fc2fc2a75b091401c1b417d5aa03c7d37467
ofnanezn <ofnanezn@gmail.com>
2026-01-15 09:46:29 -0500

    Add documentation for Q2 files

--- src/q2_memory.py
+++ src/q2_memory.py
@@ -5,19 +5,23 @@ from collections import Counter
 
 
 def q2_memory(file_path: str) -> List[Tuple[str, int]]:
-    total_emojis_counter = {}
+    total_emojis_counter = {} # Keep track of total count of emojis
     required_cols = ['content']
     
+    # Load dataframe in chunks
     for chunk in build_dataframe_chunks(file_path, required_cols):
-        chunk['content'] = chunk['content'].apply(extract_emojis)
-        chunk = chunk.explode('content')   
+        chunk['content'] = chunk['content'].apply(extract_emojis) # Get a list of emojis per content 
+        chunk = chunk.explode('content') # Flatmap operation for the list of emojis 
         
+        # Count emojis and transform to a dictionary emoji: count
         count_emojis = chunk.value_counts()
         count_emojis.index = count_emojis.index.get_level_values(0)
         count_emojis_dict = count_emojis.to_dict()
 
+        # Sum counts to the total tracker
         total_emojis_counter = dict(Counter(total_emojis_counter) + Counter(count_emojis_dict))
 
+    # Sort based on count and get top 10 emojis
     sorted_emojis = sorted(total_emojis_counter.items(), key=lambda x: x[1], reverse=True)
     return sorted_emojis[:10]
 
--- src/q2_time.py
+++ src/q2_time.py
@@ -8,8 +8,9 @@ def q2_time(file_path: str) -> List[Tuple[str, int]]:
     
     df = build_dataframe_time(file_path, required_cols)
     
-    df['content'] = df['content'].apply(extract_emojis)
-    df = df.explode('content')   
-    top_emojis = df.value_counts().sort_values(ascending=False).head(10)
+    df['content'] = df['content'].apply(extract_emojis) # Get a list of emojis per content 
+    df = df.explode('content') # Flatmap operation for the list of emojis   
+    top_emojis = df.value_counts().sort_values(ascending=False).head(10) # Get top 10 used emojis 
 
+    # return in the required format
     return list(top_emojis.reset_index().itertuples(index=None, name=None))
