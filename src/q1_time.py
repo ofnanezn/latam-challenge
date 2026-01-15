fcff6cc4fa0a3edb00c4fe72058108edccbd392d
ofnanezn <ofnanezn@gmail.com>
2026-01-14 22:33:39 -0500

    Add Q1 - Time optimized

--- src/q1_time.py
+++ src/q1_time.py
@@ -8,5 +8,12 @@ def q1_time(file_path: str) -> List[Tuple[datetime.date, str]]:
     # Assuming beforehand we know the columns we need for the exercise 
     required_cols = ['date', 'user']
     df = build_dataframe(file_path, required_cols)
-    return df
+    df['date'] = pd.to_datetime(df['date']).dt.date
+    df['counts'] = df.groupby('date')['date'].transform('count')
+    df['rank'] = df['counts'].rank(ascending=False, method='dense')
+    df = df[df['rank'] <= 10]
+    df['username'] = df['user'].str['username']
+    username_counts = df.groupby(['date', 'rank'])['username'].value_counts()
+    result = username_counts.groupby(level=0).head(1).reset_index().sort_values(by='rank')
+    return list(result[['date', 'username']].itertuples(index=False, name=None))
     
