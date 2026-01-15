bdecb8f169240a1ee46ab13955cd63e8352e76f9
ofnanezn <ofnanezn@gmail.com>
2026-01-15 01:39:37 -0500

    Add Q3 time optimized solution

--- src/q3_time.py
+++ src/q3_time.py
@@ -1,4 +1,16 @@
 from typing import List, Tuple
 
+from src.utils import build_dataframe_time
+
 def q3_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    required_cols = ["mentionedUsers"]
+
+    df = build_dataframe_time(file_path, required_cols)
+
+    df = df.explode('mentionedUsers')
+    df['mentionedUsers'] = df['mentionedUsers'].str['username']
+
+    top_users = df.value_counts().sort_values(ascending=False).head(10)
+
+    return list(top_users.reset_index().itertuples(index=None, name=None))
+
